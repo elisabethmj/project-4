979a75c68ebab64df1ee7f6f3a0a6b7a0bc23100
Elise James <elisabethmariajames@gmail.com>
2022-10-15 14:12:48 +1100

    set up done

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3001; 
+
+app.use(express.json());
+app.use(express.static("./client/build"));
+
+app.get("/api/test", (req, res) => {
+  res.json({ result: "success" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
+
+
